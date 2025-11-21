72caecfc95b5f6d24146952de100068938d0974c
rodrossi <rodrigoormonde@gmail.com>
2025-11-21 12:00:45 -0300

    Exemplos de tipos de variaveis e suas aplicações

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,15 +1,26 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class TiposVariaveis {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        // tipos primitivos
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        String meuNome = "Rodrigo Rossi";
+
+        // o ponto flutuante normalmente fica nos centavos(R$ 2.500,00)
+        double salarioMinimo = 2500.00;
+        System.out.println("R$ " + salarioMinimo);
+
+        //Tomar cuidado com os tipos de atribuições
+        short mumeroCurto = 1;
+        int numeroNormal = mumeroCurto;
+        short numereoCurto2 = (short) numeroNormal;
+
+
+        //variaveis podem sofrer mudanças
+        int numero = 5;
+        numero = 10;
+
+        System.out.println(numero);
+
+        //Constantes precisam estar com a palavra reservada "final"
+        final double VALOR_DE_PI = 3.14;
     }
 }
